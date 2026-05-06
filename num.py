ee3eab0b8c23e9d28196eeb34bb5398674edc099
Dim <disr9197@gmail.com>
2026-05-06 17:17:04 +0500

    second commit

--- num.py
+++ num.py
@@ -1,38 +1,73 @@
-def сложение(x, y):
-    return int(x + y)
+def evaluate(expression):
+    stack = []
+    operators = {'+': lambda x, y: x + y,
+                 '-': lambda x, y: x - y,
+                 '*': lambda x, y: x * y,
+                 '/': lambda x, y: x / y}
+    current_number = ''
+    for char in expression:
+        if char.isdigit() or char == '.':
+            current_number += char
+        elif char in operators:
+            if current_number:
+                stack.append(float(current_number))
+                current_number = ''
+            operator = operators[char]
+            if len(stack) >= 2:
+                y = stack.pop()
+                x = stack.pop()
+                result = operator(x, y)
+                stack.append(result)
+        elif char == '(':
+            stack.append(char)
+        elif char == ')':
+            while stack and stack[-1] != '(':
+                y = stack.pop()
+                x = stack.pop()
+                operator = stack.pop()
+                result = operator(x, y)
+                stack.append(result)
+            if stack and stack[-1] == '(':
+                stack.pop()
+        elif char == ' ':
+            continue
+        else:
+            raise ValueError("Invalid character in expression")
+    if current_number:
+        stack.append(float(current_number))
+    while len(stack) > 1:
+        y = stack.pop()
+        x = stack.pop()
+        operator = stack.pop()
+        result = operator(x, y)
+        stack.append(result)
+    return stack[0]
 
-def вычитание(x, y):
-    return int(x - y)
-
-def умножение(x, y):
-    return int(x * y)
-
-def деление(x, y):
-    if y != 0:
-        return int(x / y)
-    else:
-        return "Ошибка: Деление на ноль"
-
-print("Простой калькулятор")
+print("Расширенный калькулятор")
 print("1. Сложение")
 print("2. Вычитание")
 print("3. Умножение")
 print("4. Деление")
+print("5. Выражение")
 
-выбор = input("Введите выбор (1-4): ")
+выбор = input("Введите выбор (1-5): ")
 
-число1 = int(input("Введите первое число: "))
-число2 = int(input("Введите второе число: "))
-
-if выбор == '1':
-    результат = сложение(число1, число2)
-elif выбор == '2':
-    результат = вычитание(число1, число2)
-elif выбор == '3':
-    результат = умножение(число1, число2)
-elif выбор == '4':
-    результат = деление(число1, число2)
+if выбор == '5':
+    выражение = input("Введите выражение: ")
+    результат = evaluate(выражение)
 else:
-    результат = "Ошибка: Неверный выбор"
+    число1 = float(input("Введите первое число: "))
+    число2 = float(input("Введите второе число: "))
+
+    if выбор == '1':
+        результат = число1 + число2
+    elif выбор == '2':
+        результат = число1 - число2
+    elif выбор == '3':
+        результат = число1 * число2
+    elif выбор == '4':
+        результат = число1 / число2
+    else:
+        результат = "Ошибка: Неверный выбор"
 
 print("Результат:", результат)
